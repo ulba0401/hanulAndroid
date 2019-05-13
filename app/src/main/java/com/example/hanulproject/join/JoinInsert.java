78b5bb6ee891a42550959b597fca639b432a652c
Imhyunji <baer96@naver.com>
2019-05-13 15:54:59 +0900

    회원가입 추가

---
+++ app/src/main/java/com/example/hanulproject/join/JoinInsert.java
@@ -0,0 +1,62 @@
+package com.example.hanulproject.join;
+
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+
+import java.io.InputStream;
+
+import static com.example.hanulproject.task.common.CommonMethod.ipConfig;
+
+public class JoinInsert extends AsyncTask<Void, Void, Void> {
+
+    String jname, jid, jpw, jemail;
+
+    public JoinInsert(String name, String id, String pw, String email) {
+        this.jname = name;
+        this.jid = id;
+        this.jpw= pw;
+        this.jemail=email;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+            builder.addTextBody("name", jname, ContentType.create("Multipart/related", "UTF-8"));
+            builder.addTextBody("id", jid, ContentType.create("Multipart/related", "UTF-8"));
+            builder.addTextBody("pw", jpw, ContentType.create("Multipart/related", "UTF-8"));
+            builder.addTextBody("email", jemail, ContentType.create("Multipart/related", "UTF-8"));
+
+            String postURL = ipConfig + "/AA/uinsert";
+
+            InputStream inputStream = null;
+            HttpClient httpClient = AndroidHttpClient.newInstance("Android");
+
+            HttpPost httpPost = new HttpPost(postURL);
+            httpPost.setEntity(builder.build());
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            inputStream = httpEntity.getContent();
+
+        }catch (Exception e){
+            e.getMessage();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
+
--- app/src/main/java/com/example/hanulproject/join/Join_main.java
+++ app/src/main/java/com/example/hanulproject/join/Join_main.java
@@ -1,10 +1,15 @@
 package com.example.hanulproject.join;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -13,12 +18,18 @@ import com.example.hanulproject.login.Login;
 import com.example.hanulproject.login.Login_menu;
 import com.example.hanulproject.login.Login_page;
 
+import org.w3c.dom.Text;
+
+import static com.example.hanulproject.task.common.CommonMethod.isNetworkConnected;
+
 
 public class Join_main extends AppCompatActivity {
 
     ToggleButton checkBtn;
     Button joinBtn;
     int check = 0;
+    EditText name, id, pw, pwc, email;
+    TextView jpwcompl, jpwfail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,15 @@ public class Join_main extends AppCompatActivity {
 
         checkBtn = findViewById(R.id.checkBtn);
         joinBtn = findViewById(R.id.realJoin);
+        name =findViewById(R.id.joinName);
+        id=findViewById(R.id.joinId);
+        pw=findViewById(R.id.joinPw);
+        pwc=findViewById(R.id.joinPwr);
+        email=findViewById(R.id.joinEmail);
+        jpwcompl=findViewById(R.id.jpwcompl);
+        jpwfail=findViewById(R.id.jpwfail);
+
+
 
         checkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,18 +65,75 @@ public class Join_main extends AppCompatActivity {
             }
         });
 
+        //비밀번호 두개 비교해서 일치
+        pwc.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String password=pw.getText().toString();
+                String confirm=pwc.getText().toString();
+                if(password.equals(confirm)){
+                    jpwcompl.setVisibility(View.VISIBLE);
+                    jpwfail.setVisibility(View.GONE);
+                }else{
+                    jpwcompl.setVisibility(View.GONE);
+                    jpwfail.setVisibility(View.VISIBLE);
+                }
+                if(pw.getText().toString().length()==0 || pwc.getText().toString().length()==0){
+                    jpwcompl.setVisibility(View.GONE);
+                    jpwfail.setVisibility(View.GONE);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+
         joinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (check == 1){
 
+                if(name.getText().toString().length()==0){
+                    Toast.makeText(Join_main.this, "이름을 입력하세요.", Toast.LENGTH_SHORT).show();
+                    name.requestFocus();
+                    return;
+                }
+                if(id.getText().toString().length()==0){
+                    Toast.makeText(Join_main.this, "아이디를 입력하세요.", Toast.LENGTH_SHORT).show();
+                    id.requestFocus();
+                    return;
+                }
+                if(email.getText().toString().length()==0){
+                    Toast.makeText(Join_main.this, "이메일을 입력하세요.", Toast.LENGTH_SHORT).show();
+                    id.requestFocus();
+                    return;
+                }
+                if(pw.getText().toString().length()==0){
+                    Toast.makeText(Join_main.this, "비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show();
+                    pw.requestFocus();
+                    return;
+                }
+                if(pwc.getText().toString().length()==0){
+                    Toast.makeText(Join_main.this, "비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                    pwc.requestFocus();
+                    return;
+                }
 
+                if (isNetworkConnected(Join_main.this)==true){
                     /* 회원가입 처리할 부분 */
+                    String jname=name.getText().toString();
+                    String jid=id.getText().toString();
+                    String jpw=pw.getText().toString();
+                    String jemail=email.getText().toString();
 
 
                     Intent intent = new Intent(Join_main.this, Login_page.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    JoinInsert insert=new JoinInsert(jname, jid, jpw, jemail);
+                    insert.execute();
                     startActivity(intent);
+
                     finish();
 
                 }else{
